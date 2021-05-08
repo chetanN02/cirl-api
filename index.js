95f04da042816d5f8e83e437b5a2b47780e22c01
Chetan Naik <chetannaik02.cn@gmail.com>
2021-05-08 18:48:30 +0530

    Added models and routes

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express")
 const mongoose = require("mongoose")
 const app = express();
 require('dotenv').config()
+const washingMachineRoute = require("./routes/washingMachine")
 
 const PORT = process.env.PORT || 3000
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// routes
+app.use('/api/washingmachine',washingMachineRoute)
 
 mongoose
     .connect(
---
+++ routes/washingMachine.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router();
+const WashingMachine = require("../models/washingMachine")
+
+router.get("/",(req,res) => {
+    res.send("Hey")
+})
+
+// get
+router.get("/:barcodeId", async (req, res) => {
+    const washingmachine = await WashingMachine.findOne({
+        barcodeId: req.params.barcodeId,
+    })
+    res.json(washingmachine)
+})
+
+//post
+router.post("/", (req, res) => {
+    const washingmachine = new WashingMachine(req.body)
+    washingmachine.save().then(washingmachine=>{
+        res.send(washingmachine)
+    }).catch(err => {
+        res.status(500).send(err)
+    })
+})
+
+module.exports = router
